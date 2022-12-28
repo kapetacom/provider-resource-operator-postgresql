3a2b7aa9e80d316101cc790830ac4152f07d16a5
Henrik Hofmeister <henrik@newdawn.dk>
2022-12-28 14:01:09 +0100

    Adjusted build settings to handle multiple versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const Path = require('path');
+const FS = require('fs');
+const YAML = require('yaml');
+
+const blockInfo = YAML.parse(FS.readFileSync(Path.resolve(__dirname, './blockware.yml')).toString());
+const packageJson = require('./package.json');
 
 module.exports = {
-    mode: 'development',
     entry: {
-        'blockware/resource-type-postgresql': Path.resolve(__dirname, "./src/web")
+        [`${blockInfo.metadata.name}:${packageJson.version}`]: {
+            import: Path.resolve(__dirname, "./src/web"),
+            filename: `${blockInfo.metadata.name}.js`
+        }
     },
     output: {
         path: Path.join(process.cwd(), 'web'),
         filename: '[name].js',
-        library: `Blockware.resourceTypes["[name]"]`,
-        libraryTarget: 'assign'
+        library: {
+            name: `Blockware.resourceTypes["[name]"]`,
+            type: 'assign',
+            export: 'default'
+        }
     },
     module: {
         rules: [
@@ -23,7 +33,7 @@ module.exports = {
                         "@babel/react"
                     ],
                     plugins: [
-                        ["@babel/plugin-proposal-decorators", {legacy: true} ],
+                        ["@babel/plugin-proposal-decorators", {legacy: true}],
                         ["@babel/plugin-proposal-private-methods", {"loose": true}],
                         ["@babel/plugin-proposal-private-property-in-object", {"loose": true}],
                         ["@babel/plugin-proposal-class-properties", {loose: true}],
