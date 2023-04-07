6e429ffb9ceb0b95f4d1ed912d1a3cd2e9f95f78
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-07 22:44:25 +0200

    Update postgres to fit new system

--- src/web/PostgreSQLEditorComponent.tsx
+++ src/web/PostgreSQLEditorComponent.tsx
@@ -1,13 +1,6 @@
-import React,{Component} from "react";
-import _ from "lodash";
+import React from "react";
 
-
-import {
-    ResourceMetadata,
-    ResourceConfigProps,
-} from "@kapeta/ui-web-types";
-import {observer} from "mobx-react";
-import {FormInput} from "@kapeta/ui-web-components";
+import {FormField} from "@kapeta/ui-web-components";
 
 function validateDatabaseName(fieldName:string, name:string) {
     if (!/^[a-z]([a-z0-9_-]*[a-z0-9_])?$/i.test(name)) {
@@ -15,29 +8,16 @@ function validateDatabaseName(fieldName:string, name:string) {
     }
 }
 
-@observer
-class PostgreSQLEditorComponent extends Component<ResourceConfigProps<ResourceMetadata>> {
+const PostgreSQLEditorComponent = () => {
 
-
-    private handleMetaDataChanged(name:string, value:string) {
-        const metadata = _.clone(this.props.metadata);
-        metadata[name] = value.trim();
-        this.props.onDataChanged(metadata);
-    }
-
-    render() {
-
-        return (
-            <FormInput
-                name={"name"}
-                value={this.props.metadata.name}
-                label={"Name"}
-                validation={['required', validateDatabaseName]}
-                help={"Name your database"}
-                onChange={(name: string, input: string) => this.handleMetaDataChanged(name, input)}
-            />
-        )
-    }
+    return (
+        <FormField
+            name={"metadata.name"}
+            label={"Name"}
+            validation={['required', validateDatabaseName]}
+            help={"Name your database"}
+        />
+    )
 }
 
 export default PostgreSQLEditorComponent;
